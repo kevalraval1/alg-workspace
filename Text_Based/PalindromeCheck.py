86886d539746a1cc307dbb3accdb53309737c4d9
Keval Raval <Keval@Kevals-MacBook-Pro.local>
2021-07-20 22:46:05 -0400

    Adding comments

--- Text_Based/PalindromeCheck.py
+++ Text_Based/PalindromeCheck.py
@@ -6,5 +6,6 @@ removeList = ["!", "?", " ", ".", "...", "(", ")", "/", "&"]
 #.lower() as capitalization doesn't matter either
 checkString = "".join((palindrome[i].lower() for i in range(len(palindrome)) if i not in removeList))
 
+#can remove checkString variable and just add the .join into the if statement, but this way is more cleaner in my opinion
 if palindrome.lower() == (checkString):
     print (True)
