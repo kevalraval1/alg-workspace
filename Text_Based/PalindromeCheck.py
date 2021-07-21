19bedbaaab78d1cf5506a747484866b02560e1ad
Keval Raval <Keval@Kevals-MacBook-Pro.local>
2021-07-20 22:41:21 -0400

    Finished Algorithm

--- Text_Based/PalindromeCheck.py
+++ Text_Based/PalindromeCheck.py
@@ -1,4 +1,10 @@
-palindrome = "Racecar"
+palindrome = "Was it a car or a cat I saw?"
 
-if palindrome.lower() == ("".join(reversed(palindrome.lower()))):
+#removing common punctuations as it doesn't matter in palindromes
+removeList = ["!", "?", " ", ".", "...", "(", ")", "/", "&"]
+
+#.lower() as capitalization doesn't matter either
+checkString = "".join((palindrome[i].lower() for i in range(len(palindrome)) if i not in removeList))
+
+if palindrome.lower() == (checkString):
     print (True)
